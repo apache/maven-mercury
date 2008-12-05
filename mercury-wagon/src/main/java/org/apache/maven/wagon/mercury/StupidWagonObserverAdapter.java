03162b9299ec0f85136285d268a7c486be67cdd8
Oleg Gusakov <ogusakov@apache.org>
2008-12-05 00:34:23 +0000

    releasing mercury-1.0.0-alpha-2
    git-svn-id: https://svn.apache.org/repos/asf/maven/mercury/tags/mercury-1.0.0-alpha-2@723534 13f79535-47bb-0310-9956-ffa450edef68

---
+++ mercury-wagon/src/main/java/org/apache/maven/wagon/mercury/StupidWagonObserverAdapter.java
@@ -0,0 +1,92 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.apache.maven.wagon.mercury;
+
+import org.apache.maven.mercury.crypto.api.StreamObserver;
+import org.apache.maven.mercury.crypto.api.StreamObserverException;
+import org.apache.maven.mercury.logging.IMercuryLogger;
+import org.apache.maven.mercury.logging.MercuryLoggerManager;
+import org.apache.maven.wagon.events.TransferEvent;
+
+/**
+ *
+ *
+ * @author Oleg Gusakov
+ * @version $Id$
+ *
+ */
+public class StupidWagonObserverAdapter
+implements StreamObserver
+{
+  private static final IMercuryLogger _log = MercuryLoggerManager.getLogger( StupidWagonObserverAdapter.class ); 
+
+  byte [] buf = new byte[2048];
+
+  MercuryWagon wagon;
+  TransferEvent event;
+  
+  long length = -1L;
+  String lastModified;
+  
+  public StupidWagonObserverAdapter( MercuryWagon wagon, TransferEvent event )
+  {
+    this.wagon = wagon;
+    this.event = event;
+    if( _log.isDebugEnabled() )
+      _log.debug( "|=========-> adapter for "+event.getResource().getName()+" is created" );
+  }
+
+  public void byteReady( int b )
+  throws StreamObserverException
+  {
+    bytesReady( new byte [] { (byte)b }, 0, 1 );
+  }
+
+  public void bytesReady( byte[] b, int off, int len )
+  throws StreamObserverException
+  {
+    if( len > buf.length )
+      buf = new byte[len];
+    
+    System.arraycopy( b, off, buf, 0, len );
+    wagon.bytesReady( event, buf, len );
+  }
+
+  public long getLength()
+  {
+    return length;
+  }
+
+  public void setLength( long length )
+  {
+    this.length = length;
+_log.info( "|=-> length is "+length );
+  }
+
+  public void setLastModified(String time)
+  {
+    lastModified = time;
+  }
+
+  public String getLastModified()
+  {
+    return lastModified;
+  }
+
+}
---
+++ mercury-wagon/src/main/java/org/apache/maven/wagon/mercury/TransferEventDebugger.java
@@ -0,0 +1,76 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.apache.maven.wagon.mercury;
+
+import org.apache.maven.mercury.logging.IMercuryLogger;
+import org.apache.maven.mercury.logging.MercuryLoggerManager;
+import org.apache.maven.wagon.events.TransferEvent;
+import org.apache.maven.wagon.events.TransferListener;
+
+/**
+ *
+ *
+ * @author Oleg Gusakov
+ * @version $Id$
+ *
+ */
+public class TransferEventDebugger
+implements TransferListener
+{
+  public static final String SYSTEM_PARAMETER_DEBUG_TRANSFER_BYTES = "maven.mercury.wagon.debug.transfer.bytes";
+  private boolean debugTransferBytes = Boolean.parseBoolean( System.getProperty( SYSTEM_PARAMETER_DEBUG_TRANSFER_BYTES, "false" ) );
+
+  private static final IMercuryLogger _log = MercuryLoggerManager.getLogger( TransferEventDebugger.class ); 
+
+  public void debug( String message )
+  {
+  }
+
+  public void transferCompleted(
+      TransferEvent transferEvent )
+  {
+    _log.info("|=============>   completed: "+transferEvent.getResource().getName() );
+  }
+
+  public void transferError( TransferEvent transferEvent )
+  {
+    _log.info("|=============>   error: "+transferEvent.getResource().getName() );
+  }
+
+  public void transferInitiated( TransferEvent transferEvent )
+  {
+    _log.info("|=============>   initialized: "+transferEvent.getResource().getName() );
+  }
+
+  public void transferProgress(
+      TransferEvent transferEvent,
+      byte[] buffer,
+      int length )
+  {
+    if( debugTransferBytes )
+      _log.info("|=============>   ready "+length+" bytes : "+transferEvent.getResource().getName() );
+  }
+
+  public void transferStarted(
+      TransferEvent transferEvent )
+  {
+    _log.info("|=============>   started: "+transferEvent.getResource().getName() );
+  }
+
+}
