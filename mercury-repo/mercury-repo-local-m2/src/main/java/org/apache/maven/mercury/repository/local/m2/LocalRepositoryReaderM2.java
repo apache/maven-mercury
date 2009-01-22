a95f1e814665560ac668f430936f9f690dbc2472
Oleg Gusakov <ogusakov@apache.org>
2009-01-22 05:22:43 +0000

    oops! forgot this fix
    git-svn-id: https://svn.apache.org/repos/asf/maven/mercury/tags/mercury-1.0.0-alpha-3@736565 13f79535-47bb-0310-9956-ffa450edef68

--- mercury-repo/mercury-repo-local-m2/src/main/java/org/apache/maven/mercury/repository/local/m2/LocalRepositoryReaderM2.java
+++ mercury-repo/mercury-repo-local-m2/src/main/java/org/apache/maven/mercury/repository/local/m2/LocalRepositoryReaderM2.java
@@ -194,7 +194,7 @@ public class LocalRepositoryReaderM2
         // time stamped snapshot requested
         else if ( vq.equals( Quality.SNAPSHOT_TS_QUALITY ) )
         {
-            loc.setVersionDir( loc.getBaseVersion() + FileUtil.DASH + Artifact.SNAPSHOT_VERSION );
+            loc.setVersionDir( loc.getVersionWithoutTS() + FileUtil.DASH + Artifact.SNAPSHOT_VERSION );
         }
 
         return loc;
