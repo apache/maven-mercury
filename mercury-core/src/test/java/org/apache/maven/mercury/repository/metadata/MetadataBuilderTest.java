ea3ceb84b95feb2a93306c8d9951200086250c22
Herve Boutemy <hboutemy@apache.org>
2010-01-17 23:10:56 +0000

    [MERCURY-134] 1.0-beta-3 should be < 1.0-SNAPSHOT: forgot one more unit test
    git-svn-id: https://svn.apache.org/repos/asf/maven/mercury/trunk@900242 13f79535-47bb-0310-9956-ffa450edef68

--- mercury-core/src/test/java/org/apache/maven/mercury/repository/metadata/MetadataBuilderTest.java
+++ mercury-core/src/test/java/org/apache/maven/mercury/repository/metadata/MetadataBuilderTest.java
@@ -6,9 +6,9 @@
  *  to you under the Apache License, Version 2.0 (the
  *  "License"); you may not use this file except in compliance
  *  with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing,
  *  software distributed under the License is distributed on an
  *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -250,8 +250,8 @@ public class MetadataBuilderTest
         orderedVersions.add( "1.0.1" );
         orderedVersions.add( "1.0.3-SNAPSHOT" );
         orderedVersions.add( "1.1-M1" );
-        orderedVersions.add( "1.2.0-SNAPSHOT" );
         orderedVersions.add( "1.2.0-beta-1" );
+        orderedVersions.add( "1.2.0-SNAPSHOT" );
         orderedVersions.add( "1.2.0" );
         orderedVersions.add( "1.2.0.5-SNAPSHOT" );
         orderedVersions.add( "1.3.0-SNAPSHOT" );
